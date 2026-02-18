ef97a947b17ba1e8294526b368d1a87f902a9b39
j9smith <j.smith9103@outlook.com>
2026-02-18 19:17:22 +0000

    Loggers must now implement close fn to clean up resources

--- src/alchemy/lab/loggers/base.py
+++ src/alchemy/lab/loggers/base.py
@@ -8,9 +8,14 @@ class Logger(ABC):
     def log_scalar(self):
         raise NotImplementedError()
     
+    @abstractmethod
+    def close(self):
+        raise NotImplementedError
+    
 class CompositeLogger(Logger):
     def __init__(self):
         super().__init__()
+        
     def log_scalar(self, str, scalar, index):
         print(f"Loss: {scalar}")
         return
