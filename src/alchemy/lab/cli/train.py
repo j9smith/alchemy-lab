c512ad5a01f523fa0f7b9a58be2dfa36ba0368d3
j9smith <j.smith9103@outlook.com>
2026-02-18 19:15:31 +0000

    Added basic terminal logging and support for checkpointing

--- src/alchemy/lab/cli/train.py
+++ src/alchemy/lab/cli/train.py
@@ -8,9 +8,20 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 import alchemy.lab.training.distributed as dist
 from alchemy.lab.training.runner import TrainingRunner
 from alchemy.lab.loggers.base import CompositeLogger
+from alchemy.lab.loggers.terminal import TerminalLogger, TerminalLoggerConfig
 
 @hydra.main(version_base=None, config_path="../configs", config_name="train")
 def main(cfg: DictConfig):
+    print("\n\n"
+        " █████╗ ██╗      ██████╗██╗  ██╗███████╗███╗   ███╗██╗   ██╗\n"
+        "██╔══██╗██║     ██╔════╝██║  ██║██╔════╝████╗ ████║╚██╗ ██╔╝\n"
+        "███████║██║     ██║     ███████║█████╗  ██╔████╔██║ ╚████╔╝ \n"
+        "██╔══██║██║     ██║     ██╔══██║██╔══╝  ██║╚██╔╝██║  ╚██╔╝  \n"
+        "██║  ██║███████╗╚██████╗██║  ██║███████╗██║ ╚═╝ ██║   ██║   \n"
+        "╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝   ╚═╝   \n"
+        "                            L A B\n\n"
+
+    )
     dist.init_distributed(backend=cfg.dist.backend)
     device = dist.get_device()
     dtype = torch.float32
@@ -23,11 +34,15 @@ def main(cfg: DictConfig):
     optimiser = instantiate(cfg.optim, model=model)
     loss_fn = instantiate(cfg.loss, device=device)
 
-    logger = CompositeLogger()
+    #logger = CompositeLogger()
+    logger = TerminalLogger(TerminalLoggerConfig(
+        total_steps=cfg.train.max_steps
+    ))
 
-    #dataset = build_dataset()
     dataset = instantiate(cfg.data.dataset)
     dataloader = instantiate(cfg.data.loader, dataset=dataset)
+
+    checkpoint_manager = instantiate(cfg.checkpoints)
 
     runner = TrainingRunner(
         model=model,
@@ -35,7 +50,8 @@ def main(cfg: DictConfig):
         loss_fn=loss_fn,
         logger=logger,
         cfg=cfg,
-        device=device
+        device=device,
+        checkpoint_manager=checkpoint_manager
     )
 
     runner.train(dataloader=dataloader)
